ef22d53afbbeb2a2dd16717a2173abd99601058e
DahamLeee <dlek1011@gmail.com>
2021-01-08 01:10:12 +0900

    AppConfig convert to Spring Container

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * Application 의 구성 정보, 설정 정보
+ */
+@Configuration
 public class AppConfig {
+
     // 메서드를 드러냄으로써 역할이 명확해짐 | 아까는 안에서 new 를 해줬기 때문에 보기 힘들었음
+    @Bean
     public MemberService memberService() {
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
     // MemberRepository 역할을 눈에 볼 수 있도록 빼내어준거다.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
